f4685052066bb85593e08d97a7e1d96858e16d4f
napiaaazich-hash <napiaaazich@gmail.com>
2026-08-27 00:00:59 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -2,19 +2,19 @@ if not game:IsLoaded() then
     game.Loaded:Wait()
 end
 
-local BASE = 'https://raw.githubusercontent.com/kullaniciadin/benim-hub/main/games/'
+local BASE = 'https://raw.githubusercontent.com/napiaaazich-hash/Darkness897/main/games/'
 
 local games = {
-    [18687417158] = 'oyun1.lua',   -- Forsaken
-    [142823291]   = 'oyun2.lua',   -- MM2
+    [18687417158] = 'forsaken.lua',   -- Forsaken
+    [142823291]   = 'mm2.lua',        -- MM2
 }
 
 local file = games[game.PlaceId]
 
 if file then
-    print("[Loader] Oyun tespit edildi: " .. file)
+    print("[Loader] Game detected: " .. file)
     loadstring(game:HttpGet(BASE .. file))()
 else
-    print("[Loader] Bu oyun desteklenmiyor! ID: " .. game.PlaceId)
-    game:GetService("Players").LocalPlayer:Kick("Bu oyun desteklenmiyor! ID: " .. game.PlaceId)
+    print("[Loader] This game is not supported! ID: " .. game.PlaceId)
+    game:GetService("Players").LocalPlayer:Kick("This game is not supported! ID: " .. game.PlaceId)
 end
